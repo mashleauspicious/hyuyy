313f3e393a9a89d94604283fc7392a01ed6791cf
mashleauspicious <167050368+mashleauspicious@users.noreply.github.com>
2024-04-15 22:00:17 +0530

    Update usd1.php

--- gate/usd1.php
+++ gate/usd1.php
@@ -5,7 +5,8 @@
 
 // SK BASED API WITH RATE LIMIT BYPASSED //
 
-error_reporting(0);
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 date_default_timezone_set('America/Buenos_Aires');
 
 //================ [ FUNCTIONS & LISTA ] ===============//
